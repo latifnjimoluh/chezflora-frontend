1ea07d9fbfbd12da8f9ebf4f5a77f99a96d3d619
latifnjimoluh <159473076+latifnjimoluh@users.noreply.github.com>
2025-03-21 06:12:25 +0100

    test deploiement vercel2

---
+++ app/icons.tsx
@@ -0,0 +1,24 @@
+import {
+    ArrowRightIcon,
+    CheckCircledIcon,
+    CircleIcon,
+    Cross2Icon,
+    GithubLogoIcon,
+    MoonIcon,
+    SunIcon,
+    SpinnerIcon,
+  } from "@radix-ui/react-icons"
+  
+  export const Icons = {
+    logo: () => <p>Acme</p>,
+    arrowRight: ArrowRightIcon,
+    check: CheckCircledIcon,
+    circle: CircleIcon,
+    close: Cross2Icon,
+    github: GithubLogoIcon,
+    moon: MoonIcon,
+    sun: SunIcon,
+    spinner: SpinnerIcon,
+  }
+  
+  
